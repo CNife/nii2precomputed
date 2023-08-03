8ef4085d875d501dcc721792481a16d6649ccd2e
蔡涛 <caitao@zhejianglab.com>
2023-08-03 14:06:38 +0800

    简化command

--- convert_precomputed/__main__.py
+++ convert_precomputed/__main__.py
@@ -52,27 +52,10 @@ LOG_FORMAT = (
 )
 
 
-@app.command(help="Convert single image to precomputed")
-def single_image(
-    image_path: Path = Argument(help="Image file path", exists=True, show_default=False),
-    output_directory: Path = Argument(help="Output directory", show_default=False),
-    resolution: tuple[float, float, float] = Argument(help="resolution of x, y, z", min=0.0, default=(0.0, 0.0, 0.0)),
-    z_range: tuple[int, int] = Option(help="Z range, -1 means end", default=(0, -1)),
-    write_block_size: int = Option(help="Block size when writing precomputed", default=512),
-    resume: bool = Option(help="Resume from output_directory/work_progress.json", default=True),
-) -> None:
-    logger.info(
-        f"Converting single image to precomputed: "
-        f"image_path={str(image_path)}, output_directory={str(output_directory)}, {resolution=}, {z_range=}, "
-        f"{write_block_size=}, {resume=}"
-    )
-    image_2_precomputed(image_path, output_directory, resolution, z_range, write_block_size, resume)
-
-
 @app.command(help="Convert multiple images on z dimension to precomputed")
-def multiple_images(
-    images_directory: Path = Argument(
-        help="Image files directory", exists=True, dir_okay=True, file_okay=False, show_default=False
+def convert(
+    image_path: Path = Argument(
+        help="Image file or files directory", exists=True, dir_okay=True, file_okay=True, show_default=False
     ),
     output_directory: Path = Argument(help="Output directory", show_default=False),
     resolution: tuple[float, float, float] = Argument(help="resolution of x, y, z", min=0.0, default=(0.0, 0.0, 0.0)),
@@ -81,11 +64,13 @@ def multiple_images(
     resume: bool = Option(help="Resume from output_directory/work_progress.json", default=True),
 ) -> None:
     logger.info(
-        f"Converting multiple images to precomputed: "
-        f"images_directory={str(images_directory)}, output_directory={str(output_directory)}, {resolution=}, "
-        f"{z_range=}, {write_block_size=}, {resume=}"
+        f"Converting image to precomputed: "
+        f"image_path={str(image_path)},output_directory={str(output_directory)},{resolution=},{z_range=},"
+        f"{write_block_size=},{resume=}"
     )
-    image_2_precomputed(list_dir(images_directory), output_directory, resolution, z_range, write_block_size, resume)
+    if image_path.is_dir():
+        image_path = list_dir(image_path)
+    image_2_precomputed(image_path, output_directory, resolution, z_range, write_block_size, resume)
 
 
 @app.command(help="Show single image or multiple images info")
@@ -94,7 +79,7 @@ def show_image_info(path: Path = Argument(exists=True, show_default=False)) -> N
         image_info = read_image_info(list_dir(path))
     else:
         image_info = read_image_info(path)
-    logger.info(f"{path=}")
+    logger.info(f"path={str(path)}")
     logger.info(f"{image_info=}")
 
 
