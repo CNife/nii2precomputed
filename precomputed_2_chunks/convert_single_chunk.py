da62b81a8f9b9ada489f6adaf1d09b0f712a9f70
蔡涛 <caitao@zhejianglab.com>
2023-10-08 09:37:57 +0800

    convert_single_chunk.py支持负数的pad_block_size

--- precomputed_2_chunks/convert_single_chunk.py
+++ precomputed_2_chunks/convert_single_chunk.py
@@ -1,4 +1,5 @@
 import os.path
+from pathlib import Path
 
 import numpy as np
 import tensorstore as ts
@@ -7,11 +8,11 @@ from zimg import ZImg
 
 
 def main(
-    src_url: str,
+    src_url_or_path: str,
     out_path: str,
     out_basename: str,
     out_file_type: str,
-    block_size: int,
+    pad_block_size: int,
     x_start: int,
     x_end: int,
     y_start: int,
@@ -20,23 +21,21 @@ def main(
     z_end: int,
 ) -> str:
     # 打开TensorStore
-    dataset = open_tensorstore_to_read(src_url)
+    dataset = open_tensorstore_to_read(src_url_or_path)
 
     # 读取指定区域数据
-    region = ts.d["channel", "x", "y", "z"][
-        :1, x_start:x_end, y_start:y_end, z_start:z_end
-    ]
+    region = ts.d["channel", "x", "y", "z"][:1, x_start:x_end, y_start:y_end, z_start:z_end]
     region_data = dataset[region].read().result()
 
     # 填充到指定block_size大小
-    target_shape = (block_size,) * 3 + (1,)
-    zimg_region_data = np.zeros(target_shape, dtype=region_data.dtype, order="C")
-    zimg_region_data[
-        : region_data.shape[3],
-        : region_data.shape[2],
-        : region_data.shape[1],
-        : region_data.shape[0],
-    ] = region_data.transpose()
+    if pad_block_size > 0:
+        target_shape = (pad_block_size,) * 3 + (1,)
+        zimg_region_data = np.zeros(target_shape, dtype=region_data.dtype, order="C")
+        zimg_region_data[
+            : region_data.shape[3], : region_data.shape[2], : region_data.shape[1], : region_data.shape[0]
+        ] = region_data.transpose()
+    else:
+        zimg_region_data = region_data.transpose().copy(order="C")
 
     # 写入结果
     result_zimg = ZImg(zimg_region_data)
@@ -47,12 +46,9 @@ def main(
     return result_path
 
 
-def open_tensorstore_to_read(src_url: str) -> TensorStore:
-    ts_spec = {
-        "driver": "neuroglancer_precomputed",
-        "kvstore": {"driver": "http", "base_url": src_url},
-        "scale_index": 0,
-    }
+def open_tensorstore_to_read(src: str | Path) -> TensorStore:
+    kvstore = {"driver": "http", "base_url": src} if src.startswith("http") else {"driver": "file", "path": str(src)}
+    ts_spec = {"driver": "neuroglancer_precomputed", "kvstore": kvstore, "scale_index": 0}
     dataset = ts.open(ts_spec, read=True, write=False, create=False).result()
     return dataset
 
